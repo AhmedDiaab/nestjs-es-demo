400cb9655bab0e6e3a803d0dccfa7fd53dff5d8e
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-01-04 18:59:35 +0200

    fix(): fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { LogModule } from './log/log.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import DatabaseConfig from './database/migrations/database.config';
+import DatabaseConfig from './database/config/database.config';
 
 @Module({
   imports: [
