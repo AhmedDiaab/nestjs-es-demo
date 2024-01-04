ef8e81fce5ee1bc624eea1e357370028ac2e6885
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-01-04 15:25:33 +0200

    feat(): add elastic search module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Module({
-  imports: [],
+  imports: [ElasticsearchModule.register({
+    node: 'http://localhost:9200',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
